a78ea03ed5791d0e04a9509996f39758c6384c05
Илья <rismiass@yandex.ru>
2022-05-10 22:58:28 +0500

    list of courses

--- app/src/main/java/com/example/project/ui/course/CourseViewModel.kt
+++ app/src/main/java/com/example/project/ui/course/CourseViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.project.ui.course
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CourseViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is course Fragment"
+    }
+    val text: LiveData<String> = _text
 }
